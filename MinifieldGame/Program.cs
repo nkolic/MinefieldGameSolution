a3dd4db21d3cddfffdf4e77afeda09f1aa9f1018
nihadkolic <nihad.kolic@gmail.com>
2025-03-05 15:32:53 +0100

    Decrease number of lives to 3

--- MinifieldGame/Program.cs
+++ MinifieldGame/Program.cs
@@ -3,7 +3,7 @@
     static void Main()
     {
         Board board = new Board(8, 8, 5);
-        Player player = new Player(0, 0, 5);
+        Player player = new Player(0, 0, 3);
         GameEngine game = new GameEngine(board, player);
 
         game.StartGame();
